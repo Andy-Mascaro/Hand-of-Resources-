fdaeb50fbcb62a7f506737424aaf1831818062ed
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-16 10:51:33 -0700

    setup all data base tables will start making test, model, and controller for one of the tables next

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,99 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS planes;
+DROP TABLE IF EXISTS foods;
+DROP TABLE IF EXISTS seasons;
+DROP TABLE IF EXISTS stores;
+DROP TABLE IF EXISTS flowers;
+
+CREATE TABLE planes(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    model VARCHAR,
+    engine_count INT NOT NULL,
+    maker VARCHAR
+);
+
+CREATE TABLE foods(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    taste VARCHAR,
+    heatlhy VARCHAR
+);
+
+CREATE TABLE seasons (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    spring VARCHAR,
+    summer VARCHAR,
+    fall VARCHAR,
+    winter VARCHAR
+);
+
+CREATE TABLE stores (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    grocery VARCHAR,
+    clothes VARCHAR,
+    sports VARCHAR
+);
+
+CREATE TABLE flowers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    color VARCHAR,
+    scent VARCHAR
+    
+
+);
+
+INSERT INTO planes (
+    model,
+    engine_count,
+    maker
+)
+
+VALUES
+('Max', 2, 'Boeing'),
+('Beluga', 2, 'Airbus'),
+('Skyhawk', 1, 'Cessna');
+
+INSERT INTO foods (
+    name,
+    taste,
+    heatlhy
+)
+
+VALUES
+('Cotton Candy', 'Sweet', 'Not healthy'),
+('Peanuts', 'Salty', 'High protien'),
+('Spinach', 'Earthy', 'High in fiber');
+
+INSERT INTO seasons(
+    spring,
+    summer,
+    fall,
+    winter
+)
+
+VALUES
+('Warm', 'Hot', 'Cool','Cold'),
+('Baseball', 'Swimming', 'Football', 'Skiing'),
+('Flowers Bloom', 'Grass dies', 'Trees drop leaves', 'Ground frosts over');
+
+INSERT INTO stores (
+    grocery,
+    clothes,
+    sports
+)
+
+VALUES 
+('Albertsons', 'Kohls', 'Big Five'),
+('Safeway', 'Old Navy', 'Play it again sports'),
+('Fred Meyers', 'Target', 'GI Joes');
+
+INSERT INTO flowers (
+    name,
+    color,
+    scent
+)
+
+VALUES
+('Gardenia', 'White', 'Delicious'),
+('Freesia', 'Purple', 'Spring'),
+('Hyacinth', 'Pink', 'Sweet');
